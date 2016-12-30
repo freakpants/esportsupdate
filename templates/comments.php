27e44b132fb1b67d8d6323ebeb885a57dbd54228
freakpants <freakpants@gmail.com>
2016-12-30 01:56:10 +0000

    moved comment form after existing comments

--- templates/comments.php
+++ templates/comments.php
@@ -2,11 +2,20 @@
 if (post_password_required()) {
   return;
 }
+
+  $comment_field = '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
+  
+  $args = array(
+  "title_reply" => __( 'Hinterlass hier deinen Senf. Oder lass es bleiben.' ), "comment_field" => $comment_field , 
+  "label_submit" => __( 'Kommentar abschiessen' ) );
+  
+  comment_form( $args );
+
 ?>
 
 <section id="comments" class="comments">
   <?php if (have_comments()) : ?>
-    <h2><?php printf(_nx('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sage'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'); ?></h2>
+    <h2><?php printf(_nx('1 Kommentar', '%1$s Kommentare', get_comments_number(), 'comments title', 'sage'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'); ?></h2>
 
     <ol class="comment-list">
       <?php wp_list_comments(['style' => 'ol', 'short_ping' => true]); ?>
@@ -34,9 +43,5 @@ if (post_password_required()) {
 
   <?php 
   
-  $comment_field = '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
-  
-  $args = array("title_reply" => __( 'Hinterlass hier deinen Senf. Oder lass es bleiben.' ), "comment_field" => $comment_field , "label_submit" => __( 'Kommentar abschiessen' ));
-  
-  comment_form( $args ); ?>
+ ?>
 </section>
